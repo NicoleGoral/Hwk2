183e5f6cf088fe591a6f78c84b4200e99574cbae
Nicole Goral <78056391+NicoleGoral@users.noreply.github.com>
2022-09-12 14:22:30 -0500

    Update indexGET.php

--- indexGET.php
+++ indexGET.php
@@ -1,21 +1,10 @@
 <?php require_once("header.php"); ?>
 
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-  </head>
-  <body>
-    <h1>Hello, This is Nicole's hwk2!</h1>
-    <form action="handleGET.php" method="get">
+
+<form action="handleGET.php" method="get">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
 <input type="submit">
 </form>
-    
-    <a class="btn btn-primary" href="page2.html" role="button">Page 2</a>
 
 <?php require_once("footer.php"); ?>
